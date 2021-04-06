a94cc52045f64e06dfe9a74df81b04558648cee4
benjaminhale <33043151+benjaminhale@users.noreply.github.com>
2021-04-05 23:43:09 -0600

    Add project files.

---
+++ Components/TeamViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using BowlingContact.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingContact.Components
+{
+    public class TeamViewComponent : ViewComponent
+    {
+        private BowlingLeagueContext context;
+        public TeamViewComponent (BowlingLeagueContext ctx)
+        {
+            context = ctx;
+        }
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedType = RouteData?.Values["team"];
+
+            return View(context.Teams
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
